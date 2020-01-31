8653cb98e0df61a7069be1ad4dbd9a0471e919ca
Karla jaime <carlyc_98@hotmail.com>
2020-01-31 08:58:04 -0700

    cambios

--- PlantillaGraficos/PlantillaGraficos.cpp
+++ PlantillaGraficos/PlantillaGraficos.cpp
@@ -12,25 +12,56 @@
 
 using namespace std;
 
-float posXTriangulo = 0.0f, posYTriangulo = 0.0f;
+GLFWwindow* window;
 
-void actualizar() {
+float posXTriangulo = 0.0f, posYTriangulo = 0.0f;
+double tiempoActual, tiempoAnterior;
+double velTriangulo = 0.5;
+
+	
+
+void actualizar() {// Se puede conv. tecla pero el procesamiento es importante
 	//posXTriangulo += 0.00001;
+	
+	tiempoActual = glfwGetTime();
+	double tiempoDiferencial = tiempoActual - tiempoAnterior;
+
+	int estadoDerecha = glfwGetKey(window, GLFW_KEY_RIGHT);
+	if (estadoDerecha == GLFW_PRESS) {
+		posXTriangulo += velTriangulo * tiempoDiferencial;
+	}
+	int estadoArriba = glfwGetKey(window, GLFW_KEY_UP);
+	if (estadoArriba == GLFW_PRESS) {
+		posYTriangulo += velTriangulo * tiempoDiferencial;
+	}
+
+	int estadoIzquierda = glfwGetKey(window, GLFW_KEY_LEFT);
+	if (estadoIzquierda == GLFW_PRESS) {
+		posXTriangulo -= velTriangulo * tiempoDiferencial;
+	}
+
+	int estadoAbajo = glfwGetKey(window, GLFW_KEY_DOWN);
+	if (estadoAbajo == GLFW_PRESS) {
+		posYTriangulo -= velTriangulo * tiempoDiferencial;
+	}
+	
+	tiempoAnterior = tiempoActual;
+
 }
-//Para que responda a los eventos del teclado
+//Para que responda a los eventos del teclado, Es mas lento de esta manera y no hay conv. de teclas
 void teclado_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
 
 	if ((action == GLFW_PRESS || action == GLFW_REPEAT) && key == GLFW_KEY_RIGHT) {
 		posXTriangulo += 0.01;
 	}
-	else if ((action == GLFW_PRESS || action == GLFW_REPEAT) && key == GLFW_KEY_LEFT) {
+	if ((action == GLFW_PRESS || action == GLFW_REPEAT) && key == GLFW_KEY_LEFT) {
 		posXTriangulo -= 0.01;
 	}
-	else if ((action == GLFW_PRESS || action == GLFW_REPEAT) && key == GLFW_KEY_DOWN) {
+	if ((action == GLFW_PRESS || action == GLFW_REPEAT) && key == GLFW_KEY_UP) {
+		posYTriangulo += 0.01;
+	}
+	if ((action == GLFW_PRESS || action == GLFW_REPEAT) && key == GLFW_KEY_DOWN) {
 		posYTriangulo -= 0.01;
-	}
-	else if ((action == GLFW_PRESS || action == GLFW_REPEAT) && key == GLFW_KEY_LEFT) {
-		posXTriangulo -= 0.01;
 	}
 }
 
@@ -49,7 +80,6 @@ void dibujarTriangulo() {
 	glEnd();
 	glPopMatrix();
 
-
 }
 
 void dibujar() {
@@ -59,7 +89,7 @@ void dibujar() {
 int main()
 {
 	//Declarar una ventana 
-	GLFWwindow* window;
+	
 
 
 	//Si no se pudo inicializar GLFW  //Terminamos Ejecución 
@@ -94,7 +124,10 @@ int main()
 	cout << "Version OpenGl: " << versionGL;
 	//Ventana y funcion
 	//Establecemos que con cada evento del teclado se llama a la funcion teclado_callbak
-	glfwSetKeyCallback(window, teclado_callback);
+	//glfwSetKeyCallback(window, teclado_callback);
+
+	tiempoActual = glfwGetTime();
+	tiempoAnterior = tiempoActual;
 
 	//Clilo de dibujo (Draw loop)
 	while (!glfwWindowShouldClose(window)) {
